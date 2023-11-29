8adae4ab0cda6f0f0c1472e4ad61994ff776f2bf
Mrivera <mrivera@making.com.ar>
2023-11-29 17:19:10 -0300

    ✨

--- simpliroute/items/item.py
+++ simpliroute/items/item.py
@@ -8,8 +8,25 @@ class Item:
     reference: str
     quantity_planned: float
     
-    status: str = ""
+    status: str = "pending"
     load_2: float = 0
     load_3: float = 0
     notes: str = ""
     quantity_delivered: float = 0
+
+    def to_dict(self):
+        item_list = []
+        for item in self:
+            item_dict = {
+                'title':item.title,
+                'load':item.load,
+                'reference':item.reference,
+                'quantity_planned':item.quantity_planned,
+                'status':item.status,
+                'load_2':item.load_2,
+                'load_3':item.load_3,
+                'notes':item.notes,
+                'quantity_delivered':item.quantity_delivered
+            }
+            item_list.append(item_dict)
+        return item_list
