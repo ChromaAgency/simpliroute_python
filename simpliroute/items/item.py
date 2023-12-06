256c96d374bdf713b192f96c5cd5bc22876e51f4
Mrivera <mrivera@making.com.ar>
2023-12-06 12:40:33 -0300

    ✨

--- simpliroute/items/item.py
+++ simpliroute/items/item.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json, config
+from simpliroute.config.config import ConfigV1
+import requests
 @dataclass_json
 @dataclass
 class Item:
@@ -14,3 +16,9 @@ class Item:
     load_3: float = 0
     notes: str = ""
     quantity_delivered: float = field(default=None, metadata=config(exclude=lambda x: not x))
+
+    @classmethod    
+    def update_items(cls, config:ConfigV1, visit_id:str, update_data:dict):
+        update_url = config.get_endpoint(f"{cls.endpoint}/{visit_id}/items")
+        response = requests.put(update_url,json=update_data, headers=config.headers)
+        return response
